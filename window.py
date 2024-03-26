dc96ec723acad1b6af1b20941ea2b418b7735a54
Logan Watkins <lw2014wa@gmail.com>
2024-03-26 12:35:48 -0500

    Build out window class

--- window.py
+++ window.py
@@ -1,11 +1,22 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
-
     def __init__(self, width, height):
         self.root_widget = Tk()
         self.root_widget.title = "Root Widget"
         self.canvas = Canvas(height = height, width = width)
         self.canvas.pack()
+        self.running = False
+
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
         self.running = False
 
